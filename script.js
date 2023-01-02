7db926a834d77cc8c384ab8378a89e18468ff9d4
Amelia Cahyani <110214422+amelizyn@users.noreply.github.com>
2023-01-02 09:42:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let navbar = document.querySelector('.header .navbar');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn =>{
+    btn.onclick = () =>{
+        let src = btn.getAttribute('src');
+        document.querySelector('.about .video-container .video').src = src;
+    }
+})
